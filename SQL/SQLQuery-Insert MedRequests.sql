b2d2a1406a593f2aa6841397caa034bc21014ba9
Lital Yehuda <lital5565@gmail.com>
2023-02-24 18:14:57 +0200

    add insert MedRequests to table

--- SQL/SQLQuery-Insert MedRequests.sql
+++ SQL/SQLQuery-Insert MedRequests.sql
@@ -1,1 +1,7 @@
 insert into [MedRequests] ([cUser], [aUser], [cDep], [aDep], [medId], [reqQty], [reqStatus], [reqDate]) values (15, 3, 3, 4, 1 ,2 ,'A', '2023-02-22 23:30:29');
+insert into [MedRequests] ([cUser], [aUser], [cDep], [aDep], [medId], [reqQty], [reqStatus], [reqDate]) values (15, 3, 3, 4, 2 ,2 ,'A', '2023-02-22 23:30:30');
+insert into [MedRequests] ([cUser], [aUser], [cDep], [aDep], [medId], [reqQty], [reqStatus], [reqDate]) values (22, 3, 11, 4, 1 ,2 ,'A', '2023-02-22 23:30:31');
+insert into [MedRequests] ([cUser], [aUser], [cDep], [aDep], [medId], [reqQty], [reqStatus], [reqDate]) values (15,null , 3, null, 1 ,2 ,'W', '2023-02-22 23:30:29');
+insert into [MedRequests] ([cUser], [aUser], [cDep], [aDep], [medId], [reqQty], [reqStatus], [reqDate]) values (3,null, 3, null, 1 ,2 ,'W', '2023-02-22 23:30:29');
+insert into [MedRequests] ([cUser], [aUser], [cDep], [aDep], [medId], [reqQty], [reqStatus], [reqDate]) values (3, null, 3, null, 6 ,2 ,'D', '2023-02-22 23:30:29');
+
