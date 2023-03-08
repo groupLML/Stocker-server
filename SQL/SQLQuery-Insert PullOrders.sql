5e9931c3d072bfa3e8a5f889f0622c1734b7a26f
Lital Yehuda <lital5565@gmail.com>
2023-03-08 11:21:03 +0200

    Create SQLQuery-Insert PullOrders.sql

---
+++ SQL/SQLQuery-Insert PullOrders.sql
@@ -0,0 +1,11 @@
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,3,4,'11111','W', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (7,28,16,'11112','W', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,3,4,'11113','T', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,3,4,'11114','I', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (7,28,16,'11115','T', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (8,4,10,'11116','W', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (11,4,10,'11117','I', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (10,16,3,'11118','W', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+
+
+Select * from [Users]  
