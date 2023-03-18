4b11521e3f8f8d92273a683f33a74e56f9b9013a
Lital Yehuda <lital5565@gmail.com>
2023-03-18 15:39:31 +0200

    fixed spReadDepReqOthers

--- SQL/SQLQuery-Insert Stocks.sql
+++ SQL/SQLQuery-Insert Stocks.sql
@@ -6,5 +6,7 @@ insert into [Stocks] ([medId], [depId], [stcQty], [entryDate]) values (1, 4, 5, 
 insert into [Stocks] ([medId], [depId], [stcQty], [entryDate]) values (3, 3, 20, '2022-09-30 23:30:29');
 insert into [Stocks] ([medId], [depId], [stcQty], [entryDate]) values (3, 3, 5, '2022-09-29 23:30:29');
 insert into [Stocks] ([medId], [depId], [stcQty], [entryDate]) values (3, 4, 16, '2022-09-29 23:30:29');
+insert into [Stocks] ([medId], [depId], [stcQty], [entryDate]) values (2, 4, 3, '2022-09-29 23:30:29');
+insert into [Stocks] ([medId], [depId], [stcQty], [entryDate]) values (2, 6, 5, '2022-09-29 23:30:29');
 
 select * from Stocks
