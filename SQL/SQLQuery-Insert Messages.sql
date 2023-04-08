1000f5c077971e8420aa4b8bcd0efb6e728bd787
Lital Yehuda <lital5565@gmail.com>
2023-04-08 19:02:31 +0300

    הכנסת נתונים להודעות

---
+++ SQL/SQLQuery-Insert Messages.sql
@@ -0,0 +1,5 @@
+Select * from [Messages] 
+
+insert Into [Messages] ([userId],[msg],[msgDate]) values (5, 'Acamol - ??? ???? ?????? ?28/01/2023', GETDATE())
+insert Into [Messages] ([userId],[msg],[msgDate]) values (5, 'Paracetamol ???', GETDATE())
+insert Into [Messages] ([userId],[msg],[msgDate]) values (5, 'Acamol -???? ?????? ? Newrofen ???', GETDATE())
