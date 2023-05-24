e6d0517c408d259b864e8b550e7d050716f9975b
Lital Yehuda <lital5565@gmail.com>
2023-05-24 12:03:04 +0300

    Create Dashboard.sql

---
+++ SQL/procedures/Dashboard.sql
@@ -0,0 +1,64 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE Dashboard
+	-- Add the parameters for the stored procedure here
+	@interval smallint
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	-- SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	declare @countCurrentPO smallint, @countPreviousPO smallint, @countCurrentMR smallint, @countPreviousMR smallint,
+	        @countCurrentMRD smallint, @countPreviousMRD smallint
+
+	Select @countCurrentPO= count(*), @countPreviousPO= (select count(*)
+	                                                     from [PullOrders] as PO
+	                                                     where PO.pullDate<=(getdate()-@interval) and PO.pullDate>=getdate()-@interval*2)
+	from [PullOrders] as PO
+	where datediff(day,PO.pullDate,getdate())<=@interval
+
+
+	Select @countCurrentMR=count(*), @countPreviousMR=(select count(*)
+	                                                   from [MedRequests] MR
+	                                                   where MR.reqDate<=(getdate()-@interval) and MR.reqDate>=getdate()-@interval*2)
+	from [MedRequests] MR
+	where MR.reqStatus like 'T' and datediff(day,MR.reqDate,getdate())<=@interval
+
+
+	Select @countCurrentMRD=count(*), @countPreviousMRD=(select count(*)
+	                                                     from [MedRequests] MR
+	                                                     where MR.reqStatus like 'D' and 
+													          (MR.reqDate<=(getdate()-@interval) and MR.reqDate>=getdate()-@interval*2))
+	from [MedRequests] MR
+	where MR.reqStatus like 'D' and datediff(day,MR.reqDate,getdate())<=@interval
+
+
+	select @countCurrentPO as countCurrentPO, 
+	       @countPreviousPO as countPreviousPO, 
+		   @countCurrentMR as countCurrentMR,
+		   @countPreviousMR as countPreviousMR,
+		   @countCurrentMRD as countCurrentMRD,
+		   @countPreviousMRD as countPreviousMRD
+
+END
+GO
