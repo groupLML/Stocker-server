acd868713a2e8133196ebf551be8d143c137ac04
meitavmk96 <meitavmk96@gmail.com>
2023-06-14 10:05:29 +0300

    insert PullOrders

--- SQL/SQLQuery-Insert PullOrders.sql
+++ SQL/SQLQuery-Insert PullOrders.sql
@@ -7,4 +7,97 @@ insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], 
 insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (0,44,3,'11117','W', '2023-02-22 23:30:29','2023-02-22 23:30:29');
 
 
-Select * from [Users]  
+Select * from [Users] 
+where depId = 5
+
+Select * from [PullMedOrders]
+Select * from [PullOrders]
+
+
+delete from [PullMedOrders] where orderId = 1035
+
+Select * from [Medicines]
+
+Select * from [Users]
+where jobType = 'P'
+
+
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (8,3,3,'11111','W', '2022-09-22 23:30:29','2023-10-10 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,44,3,'11112','W', '2022-05-05 17:30:29','2023-06-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,41,3,'11113','W', '2022-05-22 23:30:29','2023-07-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (7,42,3,'11114','W', '2022-06-13 16:30:29','2023-08-14 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (11,43,3,'11115','W', '2022-02-22 23:30:29','2023-03-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (11,43,3,'11116','W', '2022-08-22 05:30:29','2023-09-05 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (53,44,3,'11117','W', '2022-06-16 23:30:29','2023-07-09 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (6,3,3,'11111','W', '2022-02-22 23:30:29','2023-08-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,44,3,'11112','W', '2022-12-22 14:30:29','2023-12-23 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,41,3,'11113','W', '2022-02-14 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (7,44,3,'11114','W', '2022-10-09 23:30:29','2023-11-24 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (10,43,3,'11115','W', '2022-03-22 12:30:29','2023-06-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (11,43,3,'11116','W', '2022-01-10 23:30:29','2023-02-28 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (53,44,3,'11117','W', '2022-09-22 23:30:29','2023-12-20 23:30:29');
+
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (8,3,3,'11111','W', '2023-02-12 10:30:29','2023-04-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,44,3,'11112','W', '2023-06-11 23:30:29','2023-06-12 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,41,3,'11113','W', '2023-05-05 18:30:29','2023-06-02 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (7,42,3,'11114','W', '2023-03-17 09:30:29','2023-03-04 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (11,43,3,'11115','W', '2023-01-25 05:30:29','2023-01-10 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (11,43,3,'11116','W', '2023-01-12 23:30:29','2023-04-04 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (53,44,3,'11117','W', '2023-02-22 23:30:29','2023-05-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (6,3,3,'11111','W', '2023-04-22 23:30:29','2023-01-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,44,3,'11112','W', '2023-04-22 12:30:29','2023-03-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,41,3,'11113','W', '2023-05-22 23:30:29','2023-04-14 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (7,44,3,'11114','W', '2023-03-22 14:30:29','2023-05-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (10,43,3,'11115','W', '2023-01-22 01:30:29','2023-05-12 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (11,43,3,'11116','W', '2023-01-22 23:30:29','2023-04-05 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (53,44,3,'11117','W', '2023-05-22 23:30:29','2023-01-06 23:30:29');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (8,4,4,'11111','W', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,15,4,'11112','T', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,15,4,'11113','W', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (7,25,4,'11114','T', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (11,30,4,'11115','W', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (11,35,4,'11116','I', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (53,45,4,'11117','W', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (6,56,4,'11111','W', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,60,4,'11112','T', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,35,4,'11113','I', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (7,25,4,'11114','T', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (10,15,4,'11115','W', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (11,4,4,'11116','I', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (53,4,4,'11117','W', '2023-02-22 23:30:29','2023-02-22 23:30:29');
+
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (8,30,4,'11111','W', '2023-02-12 10:30:29','2023-04-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,35,4,'11112','T', '2023-06-11 23:30:29','2023-06-12 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,30,4,'11113','I', '2023-05-05 18:30:29','2023-06-02 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (7,35,4,'11114','T', '2023-03-17 09:30:29','2023-03-04 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (11,45,4,'11115','W', '2023-01-25 05:30:29','2023-01-10 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (11,56,4,'11116','I', '2023-01-12 23:30:29','2023-04-04 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (53,56,4,'11117','W', '2023-02-22 23:30:29','2023-05-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (6,60,4,'11111','W', '2023-04-22 23:30:29','2023-01-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,25,4,'11112','T', '2023-04-22 12:30:29','2023-03-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (5,25,4,'11113','I', '2023-05-22 23:30:29','2023-04-14 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (7,25,4,'11114','T', '2023-03-22 14:30:29','2023-05-22 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (10,60,4,'11115','W', '2023-01-22 01:30:29','2023-05-12 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (11,4,4,'11116','I', '2023-01-22 23:30:29','2023-04-05 23:30:29');
+insert into [PullOrders] ([pUser], [nUser], [depId], [reportNum], [pullStatus], [pullDate], [lastUpdate]) values (53,4,4,'11117','W', '2023-05-22 23:30:29','2023-01-06 23:30:29');
+
+
+
+
