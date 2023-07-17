921c155c706f840501eb7e2bb485f5c3e8a8e4ff
meitavmk96 <meitavmk96@gmail.com>
2023-07-17 23:25:25 +0300

    insert

--- ‏‏SQLProd/Changes in Usages.sql
+++ ‏‏SQLProd/Changes in Usages.sql
@@ -3,15 +3,19 @@
 DELETE FROM [MedUsages] WHERE [usageId] between 45 and 46
 DELETE FROM [Usages] WHERE [usageId] between 45 and 46
 
+INSERT INTO [MedUsages] ([medId], [usageId], [useQty], [chamNum]) VALUES (4,721, 17, '1191302')
 
-DELETE FROM [MedUsages] WHERE [usageId] = 47
-DELETE FROM [Usages] WHERE [usageId] = 47
+DELETE FROM [MedUsages] WHERE [usageId] = 721 and [medId] = 4
+DELETE FROM [Usages] WHERE [usageId] = 721
 
 Select * from [Usages]
-where year(lastUpdate) =2022 and depId = 3 and MONTH(lastUpdate) = 12
+where year(lastUpdate) =2023 and depId = 3 and MONTH(lastUpdate) = 3
 
 Select * from [MedUsages]
-where [usageId] = 36
+where [usageId] = 721
+
+Select * from [MedUsages]
+where [usageId] = 38
 
 --UPDATE [PushMedOrders]
 --SET [supQty] = 7.0
