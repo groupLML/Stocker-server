fe38d2e15ed3819ed7dc21c6f11c230f68bef25a
Lital Yehuda <lital5565@gmail.com>
2023-07-18 08:43:49 +0300

    סידור

--- SQL/SQLQuery-Insert Messages.sql
+++
@@ -1,7 +0,0 @@
-Select * from [Messages] 
-
-
-insert Into [Messages] ([userId],[msg],[msgDate]) values (5, 'Acamol - צפי בעוד יומיים ב28/01/2023', GETDATE())
-insert Into [Messages] ([userId],[msg],[msgDate]) values (5, 'Paracetamol חסר', GETDATE())
-insert Into [Messages] ([userId],[msg],[msgDate]) values (5, 'Acamol -כדאי להשתמש ב Newrofen חסר', GETDATE())
-
